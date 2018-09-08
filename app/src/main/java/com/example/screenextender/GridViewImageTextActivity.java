2b6cf546f79a0f03621dd866ab3b222b3bbc58b2
Anthony Li <antli.inbox@gmail.com>
2018-09-08 12:29:54 -0400

    Custom phone grid dimensions

--- app/src/main/java/com/example/screenextender/GridViewImageTextActivity.java
+++ app/src/main/java/com/example/screenextender/GridViewImageTextActivity.java
@@ -7,10 +7,19 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
+import me.himanshusoni.quantityview.QuantityView;
+
 public class GridViewImageTextActivity extends AppCompatActivity {
 
     GridView androidGridView;
-
+    QuantityView rowsQuantityView;
+    QuantityView colsQuantityView;
+    String[] gridViewString;
+    int[] gridViewImageId;
+    CustomGridViewActivity adapterViewAndroid;
+    /*
     String[] gridViewString = {
             "Alram", "Android", "Mobile", "Website", "Profile", "WordPress",
             "Alram", "Android", "Mobile", "Website", "Profile", "WordPress",
@@ -24,12 +33,52 @@ public class GridViewImageTextActivity extends AppCompatActivity {
 
     };
 
+
+    String[] gridViewString = {
+            "Alram", "Android",
+            "Alram", "Android",
+
+    } ;
+    int[] gridViewImageId = {
+            R.drawable.ic_horizontal_phone, R.drawable.ic_horizontal_phone,
+            R.drawable.ic_horizontal_phone, R.drawable.ic_horizontal_phone,
+
+    };
+    */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gridview_image_text_example);
+        rowsQuantityView=(QuantityView)findViewById(R.id.quantityView_rows);
+        colsQuantityView=(QuantityView)findViewById(R.id.quantityView_cols);
+        generateGrid();
 
-        CustomGridViewActivity adapterViewAndroid = new CustomGridViewActivity(GridViewImageTextActivity.this, gridViewString, gridViewImageId);
+        QuantityView.OnQuantityChangeListener quantityChangeListener = new QuantityView.OnQuantityChangeListener() {
+            @Override
+            public void onQuantityChanged(int oldQuantity, int newQuantity, boolean programmatically) {
+                generateGrid();
+            }
+
+            @Override
+            public void onLimitReached() {
+
+            }
+        };
+        rowsQuantityView.setOnQuantityChangeListener(quantityChangeListener);
+        colsQuantityView.setOnQuantityChangeListener(quantityChangeListener);
+
+
+
+        /*
+        int numPhones = rowsQuantityView.getQuantity()*colsQuantityView.getQuantity();
+        gridViewString = new String[numPhones];
+        Arrays.fill(gridViewString, "Select...");
+        gridViewImageId = new int[numPhones];
+        Arrays.fill(gridViewImageId, R.drawable.ic_horizontal_phone);
+        adapterViewAndroid = new CustomGridViewActivity(GridViewImageTextActivity.this, gridViewString, gridViewImageId);
+
+        androidGridView.setNumColumns(colsQuantityView.getQuantity());
         androidGridView=(GridView)findViewById(R.id.grid_view_image_text);
         androidGridView.setAdapter(adapterViewAndroid);
         androidGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -40,6 +89,33 @@ public class GridViewImageTextActivity extends AppCompatActivity {
                 Toast.makeText(GridViewImageTextActivity.this, "GridView Item: " + gridViewString[+i], Toast.LENGTH_LONG).show();
             }
         });
+        */
+
 
     }
+
+    private void generateGrid() {
+
+        int numPhones = rowsQuantityView.getQuantity()*colsQuantityView.getQuantity();
+        gridViewString = new String[numPhones];
+        Arrays.fill(gridViewString, "Select...");
+        gridViewImageId = new int[numPhones];
+        Arrays.fill(gridViewImageId, R.drawable.ic_horizontal_phone);
+
+        adapterViewAndroid = new CustomGridViewActivity(GridViewImageTextActivity.this, gridViewString, gridViewImageId);
+
+        androidGridView=(GridView)findViewById(R.id.grid_view_image_text);
+        androidGridView.setNumColumns(colsQuantityView.getQuantity());
+        androidGridView.setAdapter(adapterViewAndroid);
+        androidGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int i, long id) {
+                Toast.makeText(GridViewImageTextActivity.this, "GridView Item: " + gridViewString[+i], Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
 }
