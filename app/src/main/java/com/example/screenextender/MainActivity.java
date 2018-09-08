c7d9df278e3c55f9f430c9bc8428ad62e4d4d97d
kjiang12 <7127959+kjiang12@users.noreply.github.com>
2018-09-08 02:59:16 -0400

    Initial Join Layout

--- app/src/main/java/com/example/screenextender/MainActivity.java
+++ app/src/main/java/com/example/screenextender/MainActivity.java
@@ -14,8 +14,10 @@ public class MainActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         //Remove notification bar
-       // this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_join);
     }
+
+
 }
