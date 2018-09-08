5ef435c6aa1ae25046086168d1e7d03faeada23e
Anthony Li <antli.inbox@gmail.com>
2018-09-08 19:34:03 -0400

    Register new clients with server

--- app/src/main/java/com/example/screenextender/ClientActivity.java
+++ app/src/main/java/com/example/screenextender/ClientActivity.java
@@ -2,10 +2,12 @@ package com.example.screenextender;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
@@ -29,16 +31,25 @@ import com.google.android.gms.nearby.messages.Message;
 import com.google.android.gms.nearby.messages.MessageListener;
 
 import static android.content.pm.ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import java.net.URISyntaxException;
 
 public class ClientActivity extends AppCompatActivity {
-
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket("http://infiniscreen.herokuapp.com");
+        } catch (URISyntaxException e) {}
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Remove notification bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+        mSocket.connect();
         setRequestedOrientation(SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_client);
     }
@@ -67,13 +78,46 @@ public class ClientActivity extends AppCompatActivity {
         super.onStop();
     }
 
-    
+    public static String getDeviceName() {
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        if (model.startsWith(manufacturer)) {
+            return capitalize(model);
+        }
+        return capitalize(manufacturer) + " " + model;
+    }
+
+    private static String capitalize(String str) {
+        if (TextUtils.isEmpty(str)) {
+            return str;
+        }
+        char[] arr = str.toCharArray();
+        boolean capitalizeNext = true;
+
+        StringBuilder phrase = new StringBuilder();
+        for (char c : arr) {
+            if (capitalizeNext && Character.isLetter(c)) {
+                phrase.append(Character.toUpperCase(c));
+                capitalizeNext = false;
+                continue;
+            } else if (Character.isWhitespace(c)) {
+                capitalizeNext = true;
+            }
+            phrase.append(c);
+        }
+
+        return phrase.toString();
+    }
+
     protected void setCompleted(String value){
         ProgressBar loadingBar = findViewById(R.id.join_loading);
         loadingBar.setVisibility(View.GONE);
 
         TextView description = findViewById(R.id.join_text);
         description.setText("Host Found!");
+
+        // Register this client with the server
+        mSocket.emit("client_join", Integer.parseInt(value), getDeviceName());
 
         TextView id = findViewById(R.id.id_text);
         id.setVisibility(View.VISIBLE);
