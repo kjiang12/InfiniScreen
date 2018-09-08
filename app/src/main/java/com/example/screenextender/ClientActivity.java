699b9baec49fe4b9fc82a8f0b1e7d2c6e0fb8b26
kjiang12 <7127959+kjiang12@users.noreply.github.com>
2018-09-08 04:23:46 -0400

    join layout and main layout connection

--- app/src/main/java/com/example/screenextender/ClientActivity.java
+++ app/src/main/java/com/example/screenextender/ClientActivity.java
@@ -1,38 +1,34 @@
 package com.example.screenextender;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import static android.content.pm.ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
 
-public class MainActivity extends AppCompatActivity {
+public class ClientActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         //Remove notification bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.activity_main);
-    }
-
-    protected void switchToHost(View v){
-
-    }
-
-    protected void switchToClient(View v){
-        Intent intent = new Intent(this, ClientActivity.class);
-        startActivity(intent);
+        setRequestedOrientation(SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_join);
     }
 
 
-
+    protected void setCompleted(int value){
+        ProgressBar loadingBar = findViewById(R.id.join_loading);
+        loadingBar.setVisibility(View.GONE);
+        TextView description = findViewById(R.id.join_text);
+        description.setText("Host Found!");
+        TextView id = findViewById(R.id.id_text);
+        id.setVisibility(View.VISIBLE);
+        id.setText(value + "");
+    }
 }
