65368b4433b37d17d492df2394bc1be2ec0b96ee
Anthony Li <antli.inbox@gmail.com>
2018-09-08 20:29:32 -0400

    Retrive client list on registration completion

--- app/src/main/java/com/example/screenextender/HostActivity.java
+++ app/src/main/java/com/example/screenextender/HostActivity.java
@@ -3,6 +3,9 @@ package com.example.screenextender;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.nearby.Nearby;
 import com.google.android.gms.nearby.messages.Message;
@@ -11,17 +14,25 @@ import com.github.nkzawa.socketio.client.Socket;
 import com.github.nkzawa.emitter.Emitter;
 
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 
 public class HostActivity extends AppCompatActivity {
     Message mMessage;
+
+    Button allClientsJoinedBtn;
     private Socket mSocket;
     {
         try {
             mSocket = IO.socket("http://infiniscreen.herokuapp.com");
         } catch (URISyntaxException e) {}
     }
+    private ArrayList<String> clientNames = new ArrayList<>();
 
     private Emitter.Listener onJoinCodeReceived = new Emitter.Listener() {
         @Override
@@ -36,13 +47,46 @@ public class HostActivity extends AppCompatActivity {
         }
     };
 
+    private Emitter.Listener onClientListReceived = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    JSONObject data = (JSONObject) args[0];
+                    try {
+                        JSONArray clients = data.getJSONArray("clients");
+                        for (int i = 0; i < clients.length(); i++) {
+                            JSONObject thisClient = (JSONObject) clients.get(i);
+                            clientNames.add(thisClient.getString("name"));
+                        }
+                        Log.d("antli", clientNames.toString());
+
+
+                        Log.d("antli", "retrieved clients");
+                    } catch (JSONException e) {
+                        return;
+                    }
+                }
+            });
+        }
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle bundle) {
         super.onCreate(bundle);
+
         mSocket.on("code_assignment", onJoinCodeReceived);
+        mSocket.on("clients", onClientListReceived);
         mSocket.connect();
         mSocket.emit("host_start");
         setContentView(R.layout.activity_host_broadcasting);
+        allClientsJoinedBtn = findViewById(R.id.client_join_finished_btn);
+        allClientsJoinedBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mSocket.emit("registration_complete");
+            }
+        });
     }
 
     @Override
